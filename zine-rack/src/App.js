a2298df7ce09e21987c7879947fa6e8c5e4ae08f
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-02-03 12:08:45 -0600

    add necessary imports and const for pdf
    adding Document doesn't work yet so need that figured out

--- zine-rack/src/App.js
+++ zine-rack/src/App.js
@@ -4,12 +4,53 @@ import ReactDOM from "react-dom";
 import HTMLFlipBook from "react-pageflip";
 import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf"; //can hold page count too
 
+const samplePDF = "../public/zine-pdfs/Pink-Red-Cute-Cherub-Galentines-Day-Party-Sample-Zine.pdf";
+//pdfjs use here
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
+const width = 300;
+const height = 424;
+
+const Page = React.forwardRef(({ pageNumber }, ref) => {
+  return (
+    <div ref={ref}>
+      <ReactPdfPage pageNumber={pageNumber} width={width} />
+    </div>
+  );
+});
+
 function App() {
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="magazine-rack">
-          <div>test</div>
+          <div className='Book-zine'>
+            {/* <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+              <HTMLFlipBook
+                width={width}
+                height={height}
+                size="fixed"
+                showCover={true}
+                maxShadowOpacity={0.5}
+                mobileScrollSupport={true}
+              >
+                {Array.apply(null, Array(numPages))
+                  .map((x, i) => i + 1)
+                  .map((page) => (
+                    <Page pageNumber={page} />
+                  ))}
+              </HTMLFlipBook>
+            </Document> */}
+          </div>
           <div>test</div>
           <div>test</div>
         </div>
