f85f7aa4a7bc23c00cdaa71a706af26a9f4a027e
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-02-06 11:36:17 -0600

    move react pdf main stuff to zine reader component
    now need to abstract hardcoded pdf to use zine reader as a template

--- zine-rack/src/components/ZineReader.js
+++ zine-rack/src/components/ZineReader.js
@@ -5,11 +5,75 @@ import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf"; //can hold pa
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';//for rendering
 import 'react-pdf/dist/Page/TextLayer.css';//text slection
+//import 'core-js/es/global-this';
+// Add this at the very top of your app
+const originalURL = global.URL;
+global.URL = class URL extends originalURL {
+  static parse(url, base) {
+    try {
+      return new URL(url, base);
+    } catch {
+      return null;
+    }
+  }
+};
+
+const samplePDF = "/zine-pdfs/Pink-Red-Cute-Cherub-Galentines-Day-Party-Sample-Zine.pdf";
+//pdfjs use here
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+//pdfjs.GlobalWorkerOptions.workerSrc =
+const width = 300;
+const height = 424;
+
+const Page = React.forwardRef(({ pageNumber }, ref) => {
+  return (
+    <div ref={ref}>
+      <ReactPdfPage pageNumber={pageNumber} width={width} />
+    </div>
+  );
+});
 
 function ZineReader(){
+    const [numPages, setNumPages] = useState(null);
+    
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+      
     return(
         <>
-
+            <div className="magazine-rack">
+                <div className='Book-zine'>
+                    <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+                    <HTMLFlipBook
+                        width={width}
+                        height={height}
+                        size="fixed"
+                        showCover={true}
+                        maxShadowOpacity={0.5}
+                        mobileScrollSupport={true}
+                    >
+                        {Array.apply(null, Array(numPages))
+                        .map((x, i) => i + 1)
+                        .map((page) => (
+                            <Page pageNumber={page} />
+                        ))}
+                        {/* {Array.from(new Array(numPages), (el, index) => (
+                        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                        ))} */}
+                        {/* {new Array(numPages)
+                        .fill(0)
+                        .map((_, index) => (
+                            <div key={index}>
+                            <Page number={index + 1} />
+                            </div>
+                        ))} */}
+                    </HTMLFlipBook>
+                    </Document>
+                </div>
+                <div>test</div>
+                <div>test</div>
+            </div>
         </>
     );
 }
