d452716670e3977065591c8fd3c72e2866d30482
Anh Nguyen <vnblacksheep@gmail.com>
2026-02-09 13:09:08 -0600

    created responsive nav

--- zine-rack/src/App.js
+++ zine-rack/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <div className="magazine-rack">
           <div>test</div>
